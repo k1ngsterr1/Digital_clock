6dcfa04e876d092128f8088cef05770db18cd794
Ruslan <ruslanmakhmatov@gmail.com>
2022-06-16 14:51:41 +0600

    removed welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -6,7 +6,8 @@ window.title('Digital Clock')
 window.geometry('600x300')
 window.configure(bg='steelblue')
 
-label = Label(window, text='Welcome!', font=('Arial Black', 78, 'bold'), bg='steelblue', fg='white')
+label = Label(window, font=('Arial Black', 78, 'bold'),
+              bg='steelblue', fg='white')
 label.pack(pady=100)
 
 window.mainloop()
